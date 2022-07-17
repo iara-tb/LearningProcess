f442262138416af4a526929b546baa25092ac14d
Iara T.Bellodi <100525143+iara-tb@users.noreply.github.com>
2022-07-17 19:00:47 -0300

    Mensagem  horário - Exercícios Método DIO
    Exercício de Método que exibe mensagem de acordo com o horário do dia.

---
+++ MensagemExercícioMétodo.java
@@ -0,0 +1,15 @@
+
+public class MensagemExercícioMétodo {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Exercício Mensagem");
+		Mensagem.obterMensagem(9);
+		Mensagem.obterMensagem(14);
+		Mensagem.obterMensagem(1);
+		
+
+	}
+
+}
